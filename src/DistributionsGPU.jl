ba770face033a53263ab202eb19509378fdcfc78
Srinjoy Ganguly <srinjoyganguly@gmail.com>
2020-05-09 18:57:25 +0100

    Path changes

--- src/DistributionsGPU.jl
+++ src/DistributionsGPU.jl
@@ -4,7 +4,7 @@ using CUDAnative, CuArrays, CUDAdrv
 using Distributions
 using Distributions: ContinuousUnivariateDistribution
 
-include("src/univariate/continuous/normal.jl")
+include("univariate/continuous/normal.jl")
 
 export GPUNormal, logpdfGPU
 
