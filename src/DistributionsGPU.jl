c9a7c45c179f71e34046f4a63ee14721afb6b3bc
Srinjoy Ganguly <srinjoyganguly@gmail.com>
2020-05-09 18:18:39 +0100

    Added GPU Support for Normal
    Modified DistributionsGPU.jl and normal.jl to add GPU support for logpdf function of Normal distribution

--- src/DistributionsGPU.jl
+++ src/DistributionsGPU.jl
@@ -2,5 +2,10 @@ module DistributionsGPU
 
 using CUDAnative, CuArrays, CUDAdrv
 using Distributions
+using Distributions: ContinuousUnivariateDistribution
+
+include("normal.jl")
+
+export GPUNormal, logpdfGPU
 
 end # module
